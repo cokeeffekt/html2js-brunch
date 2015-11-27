062d9b26ad2962a2b47feeac26c37146bdf598a8
Patrik Hartwig <patrikhartwig@Patriks-MacBook.local>
2015-11-27 22:04:16 +1000

    fix build breaking node4

--- html2js.js
+++ html2js.js
@@ -4,142 +4,110 @@ var minify = require('html-minifier').minify;
 var pathUtils = require('path');
 var fs = require('fs');
 
-module.exports = function() {
-    Html2Js.prototype.brunchPlugin = true;
-    Html2Js.prototype.type = 'template';
-    Html2Js.prototype.extension = 'tpl.html';
+module.exports = function () {
+  Html2Js.prototype.brunchPlugin = true;
+  Html2Js.prototype.type = 'template';
+  Html2Js.prototype.extension = 'tpl.html';
 
-    Html2Js.prototype.compile = function(content, path, callback) {
-        var options = this.options;
-        var moduleName = normalizePath(pathUtils.relative(options.base, path));
+  Html2Js.prototype.compile = function (content, path, callback) {
+    var options = this.options;
+    var moduleName = normalizePath(pathUtils.relative(options.base, path));
 
-        //Hack to not include any files not in the app dir since brunch passes in every file matching the extension
-        if (moduleName.indexOf('..') == -1) {
-            this.moduleNames.push("'" + moduleName + "'");
+    if (moduleName.indexOf('..') == -1) {
+      this.moduleNames.push("'" + moduleName + "'");
 
-            if (options.target === 'js') {
-                return callback(null, compileTemplate(moduleName, content, options.quoteChar, options.indentString, options.useStrict, options.htmlmin));
-            } else {
-                return callback('Unknown target "' + options.target + '" specified');
-            }
-        }
+      if (options.target === 'js') {
+        return callback(null, compileTemplate(moduleName, content, options.quoteChar, options.indentString, options.useStrict, options.htmlmin));
+      } else {
+        return callback('Unknown target "' + options.target + '" specified');
+      }
+    }
 
-        return callback(null, null);
+    return callback(null, null);
+  };
+
+  Html2Js.prototype.onCompile = function (generatedFiles) {
+    var bundle = '';
+    var options = this.options;
+    var joinToKeys = Object.keys(this.joinTo);
+
+    for (var i = 0; i < joinToKeys.length; i++) {
+      var path = this.publicPath + pathUtils.sep + joinToKeys[i];
+      var targetModule = pathUtils.basename(path, '.js');
+      bundle = "templates = {}";
+      if (options.target === 'js') {
+        bundle += ';';
+      }
+
+      bundle += '\n\n';
+
+      fs.writeFile(path, bundle.concat(fileContent), function (err) {
+        if (err) throw err;
+      });
+    }
+  };
+
+  function Html2Js(cfg) {
+    cfg = cfg || {};
+    this.options = {
+      base: 'src',
+      quoteChar: '"',
+      indentString: '  ',
+      target: 'js',
+      htmlmin: {}
     };
+    this.joinTo = cfg.files ? cfg.files.templates.joinTo : null;
+    this.publicPath = cfg.paths ? cfg.paths.public : null;
+    this.moduleNames = [];
 
-    Html2Js.prototype.onCompile = function(generatedFiles) {
-        var bundle = '';
-        var options = this.options;
-        var joinToKeys = Object.keys(this.joinTo);
+    var config = cfg.plugins && cfg.plugins.html2js;
+    if (config) {
+      var options = config.options || {};
 
-        for (var i = 0; i < joinToKeys.length; i++) {
-            var path = this.publicPath + pathUtils.sep + joinToKeys[i];
-            var targetModule = pathUtils.basename(path, '.js');
-            bundle = "templates = {}";
-            if (options.target === 'js') {
-                bundle += ';';
-            }
-
-            bundle += '\n\n';
-
-            var fileContent = fs.readFileSync(path, {encoding: 'utf-8'});
-
-            if (fileContent.indexOf(bundle) == -1) {
-                fs.writeFile(path, bundle.concat(fileContent), function(err) {
-                    if (err) throw err;
-                });
-            }
+      for (var key in options) {
+        if (options.hasOwnProperty(key)) {
+          this.options[key] = options[key];
         }
-    };
+      }
+    }
+  };
 
-    function Html2Js(cfg) {
-        cfg = cfg || {};
-        this.options = {
-            base: 'src',
-            quoteChar: '"',
-            indentString: '    ',
-            target: 'js',
-            htmlmin: {}
-        };
-        this.joinTo = cfg.files ? cfg.files.templates.joinTo : null;
-        this.publicPath = cfg.paths ? cfg.paths.public : null;
-        this.moduleNames = [];
+  function escapeContent(content, quoteChar, indentString) {
+    var bsRegexp = new RegExp('\\\\', 'g');
+    var quoteRegexp = new RegExp('\\' + quoteChar, 'g');
+    var nlReplace = '\\n' + quoteChar + ' +\n' + indentString + indentString + quoteChar;
+    return content.replace(bsRegexp, '\\\\').replace(quoteRegexp, '\\' + quoteChar).replace(/\r?\n/g, nlReplace);
+  }
 
-        var config = cfg.plugins && cfg.plugins.html2js;
-        if (config) {
-            var options = config.options || {};
+  function normalizePath(p) {
+    if (pathUtils.sep !== '/') {
+      p = p.replace(/\\/g, '/');
+    }
+    return p;
+  }
 
-            for (var key in options) {
-                if (options.hasOwnProperty(key)) {
-                    this.options[key] = options[key];
-                }
-            }
-        }
-    };
+  function getContent(content, quoteChar, indentString, htmlmin) {
+    if (Object.keys(htmlmin).length) {
+      var optionArray = [];
+      for (var i in htmlmin) {
+        optionArray.push([i, htmlmin[i]]);
+      }
 
-    function escapeContent(content, quoteChar, indentString) {
-        var bsRegexp = new RegExp('\\\\', 'g');
-        var quoteRegexp = new RegExp('\\' + quoteChar, 'g');
-        var nlReplace = '\\n' + quoteChar + ' +\n' + indentString + indentString + quoteChar;
-        return content.replace(bsRegexp, '\\\\').replace(quoteRegexp, '\\' + quoteChar).replace(/\r?\n/g, nlReplace);
+      content = minify(content, optionArray);
     }
 
-    // convert Windows file separator URL path separator
+    return escapeContent(content, quoteChar, indentString);
+  }
 
-    function normalizePath(p) {
-        if (pathUtils.sep !== '/') {
-            p = p.replace(/\\/g, '/');
-        }
-        return p;
-    }
+  function compileTemplate(moduleName, content, quoteChar, indentString, useStrict, htmlmin, process) {
+    var contentModified = getContent(content, quoteChar, indentString, htmlmin, process);
+    var doubleIndent = indentString + indentString;
+    var strict = (useStrict) ? indentString + quoteChar + 'use strict' + quoteChar + ';\n' : '';
 
-    function getContent(content, quoteChar, indentString, htmlmin) {
-        if (Object.keys(htmlmin).length) {
-            var optionArray = [];
-            for (var i in htmlmin) {
-                optionArray.push([i, htmlmin[i]]);
-            }
+    var module = "module.exports = " + quoteChar + contentModified + quoteChar + ';\n\n';
 
-            content = minify(content, optionArray);
-        }
+    return module;
+  }
 
-        return escapeContent(content, quoteChar, indentString);
-    }
-
-    // compile a template to an angular module
-
-    function compileTemplate(moduleName, content, quoteChar, indentString, useStrict, htmlmin, process) {
-        var contentModified = getContent(content, quoteChar, indentString, htmlmin, process);
-        var doubleIndent = indentString + indentString;
-        var strict = (useStrict) ? indentString + quoteChar + 'use strict' + quoteChar + ';\n' : '';
-
-        // var module = 'angular.module(' + quoteChar + moduleName +
-        //     quoteChar + ', []).run([' + quoteChar + '$templateCache' + quoteChar + ', function($templateCache) ' +
-        //     '{\n' + strict + indentString + '$templateCache.put(' + quoteChar + moduleName + quoteChar + ',\n' + doubleIndent + quoteChar + contentModified +
-        //     quoteChar + ');\n}]);\n';
-
-        // var module = 'angular.module(' + quoteChar + moduleName +
-        //     quoteChar + ', []).run([' + quoteChar + '$templateCache' + quoteChar + ', function($templateCache) ' +
-        //     '{\n' + strict + indentString + '$templateCache.put(' + quoteChar + moduleName + quoteChar + ',\n' + doubleIndent + quoteChar + contentModified +
-        //     quoteChar + ');\n}]);\n';
-        var module = "module.exports = " + quoteChar + contentModified + quoteChar+';\n\n';
-
-        return module;
-    }
-
-    // compile a template to an angular module
-
-    // function compileCoffeeTemplate(moduleName, content, quoteChar, indentString, htmlmin) {
-    //     var contentModified = getContent(content, quoteChar, indentString, htmlmin);
-    //     var doubleIndent = indentString + indentString;
-    //
-    //     var module = 'angular.module(' + quoteChar + moduleName +
-    //         quoteChar + ', []).run([' + quoteChar + '$templateCache' + quoteChar + ', ($templateCache) ->\n' +
-    //         indentString + '$templateCache.put(' + quoteChar + moduleName + quoteChar + ',\n' + doubleIndent + quoteChar + contentModified +
-    //         quoteChar + ')\n])\n';
-    //
-    //     return module;
-    // }
-
-    return Html2Js;
+  return Html2Js;
 }();
